e3a8096c08bc1d4f4b41982f7ebdb2be43bb26d2
Ilja M <i.meleshkevich@gmail.com>
2022-05-25 23:47:06 +0200

    added tech stack section

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-// A bit of JQuery 
+// A bit of JQuery
 
-$(document).ready(function(){
-    $('.header').height($(window).height());
-   })
+$(document).ready(function () {
+  $(".header").height($(window).height());
+});
 
 // Scrol
-$(".navbar a").click(function(){
-    $("body,html").animate({
-     scrollTop:$("#" + $(this).data("value")).offset().top
-    },1000)
-    
-   })
+$(".navbar a").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: $("#" + $(this).data("value")).offset().top,
+    },
+    3000
+  );
+});
 
-   // Navbar hide onscroll
+// Navbar hide onscroll
 let prevScrollpos = window.scrollY;
 window.onscroll = () => {
   hideNavbar();
   scrollFunction();
-} 
+};
 
 function hideNavbar() {
   let currentScrollPos = window.scrollY;
